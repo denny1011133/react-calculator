da5663cfc8f7634d98c3845e9e0518336fcfb922
Denny <denny1011133@luci.group>
2022-02-18 17:32:34 +0800

    adjust Wrapper.css

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -5,22 +5,15 @@ import { useDrag } from 'react-dnd'
 
 const Wrapper = ({ children }) => {
 
-
     const [, dragRef] = useDrag(() => ({
         type: ItemTypes.WRAPPER,
-       
     }))
-
-
 
     return (
         <div ref={dragRef} className="wrapper">{children}</div>)
 };
 
 
-
-
-
-
-
 export default Wrapper;
+
+

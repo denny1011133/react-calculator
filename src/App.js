abe98a7d04cad95239bda419ce33c9d166418580
Denny <denny1011133@gmail.com>
2022-02-19 20:09:18 +0800

    debug rerender in handleSubmit

--- src/App.js
+++ src/App.js
@@ -3,289 +3,291 @@ import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
 import ButtonArea from "./components/ButtonArea";
 import Button from "./components/Button";
-import { ItemTypes } from "./constants"
-import { useDrop } from 'react-dnd'
+import { ItemTypes } from "./constants";
+import { useDrop } from "react-dnd";
 
-const btnSymbols = ["C", "+-", "%", "sq", 7, 8, 9, "÷", 4, 5, 6, "×", 1, 2, 3, "-", 0, ".", "=", "+"]
-
+const btnSymbols = [
+  "C",
+  "+-",
+  "%",
+  "sq",
+  7,
+  8,
+  9,
+  "÷",
+  4,
+  5,
+  6,
+  "×",
+  1,
+  2,
+  3,
+  "-",
+  0,
+  ".",
+  "=",
+  "+",
+];
 
 function App() {
+  const enteredValue = localStorage.getItem("enteredValue");
+  const totalValue = localStorage.getItem("totalValue");
+  const sign = localStorage.getItem("sign");
 
-  const enteredValue = localStorage.getItem('enteredValue')
-  const totalValue = localStorage.getItem('totalValue')
-  const sign = localStorage.getItem('sign')
   let [count, setCount] = useState({
     enteredValue: Number(enteredValue) ? Number(enteredValue) : 0,
     totalValue: Number(totalValue) ? Number(totalValue) : 0,
     sign: sign ? sign : "",
-  })
+  });
 
-  const [, dropRef] = useDrop(
-    () => ({
-      accept: ItemTypes.WRAPPER,
-      drop: (item) => {
-        console.log("Drop on todo", item);
-      },
-    })
-  )
+  const [, dropRef] = useDrop(() => ({
+    accept: ItemTypes.WRAPPER,
+    drop: (item) => {
+      console.log("Drop on todo", item);
+    },
+  }));
 
   const handleClick = (btn) => {
     switch (btn) {
       case "=":
-        handleSubmit()
+        handleSubmit();
         break;
       case "+":
       case "-":
       case "÷":
       case "×":
-        handleSignClick(btn)
+        handleSignClick(btn);
         break;
       case "C":
-        handleClear()
+        handleClear();
         break;
       case "+-":
-        handleReverse()
+        handleReverse();
         break;
       case "%":
-        handlePercent()
+        handlePercent();
         break;
       case "sq":
-        handleSqrt()
+        handleSqrt();
         break;
       case ".":
-        handleDecimal(btn)
+        handleDecimal(btn);
         break;
       default:
-        handleNumberClick(btn)
+        handleNumberClick(btn);
     }
-
-
-  }
+  };
 
   const handleNumberClick = (btnSymbol) => {
-
-    btnSymbol = btnSymbol.toString()
+    btnSymbol = btnSymbol.toString();
 
     function handleEnteredValue() {
-
       if (count.enteredValue === 0 && btnSymbol === "0") {
-        return "0"
+        return "0";
       } else if (count.enteredValue % 1 === 0) {
-
-        return Number(count.enteredValue + btnSymbol)
-
+        return Number(count.enteredValue + btnSymbol);
       }
 
-      return count.enteredValue + btnSymbol
-
+      return count.enteredValue + btnSymbol;
     }
 
     function handleTotalValue() {
-      return !count.sign ? 0 : count.totalValue
+      return !count.sign ? 0 : count.totalValue;
     }
 
-    localStorage.setItem('enteredValue', handleEnteredValue());
-    localStorage.setItem('totalValue', handleTotalValue());
+    localStorage.setItem("enteredValue", handleEnteredValue());
+    localStorage.setItem("totalValue", handleTotalValue());
 
     setCount({
       ...count,
       enteredValue: handleEnteredValue(),
-      totalValue: handleTotalValue()
+      totalValue: handleTotalValue(),
     });
-
-  }
+  };
 
   const handleSubmit = () => {
     if (count.sign && count.enteredValue) {
-
       const arithmetic = (a, b, sign) => {
-
-
         if (sign === "+") {
-          if (a + b > Math.pow(2, 32)) {
-            alert("超過數字上限")
-            return 0
+          function isInt(n) {
+            return n % 1 === 0;
           }
-          return a + b
-
-
+          if (a + b > Math.pow(2, 32)) {
+            alert("超過數字上限");
+            return 0;
+          } else if (!isInt(a) && !isInt(b)) {
+            return (a * 10 + b * 10) / 10;
+          }
+          return a + b;
         } else if (sign === "-") {
           if (a - b > Math.pow(2, 32)) {
-            alert("超過數字上限")
-            return 0
+            alert("超過數字上限");
+            return 0;
           }
-          return a - b
-
+          return a - b;
         } else if (sign === "×") {
           if (a * b > Math.pow(2, 32)) {
-            alert("超過數字上限")
-            return 0
+            alert("超過數字上限");
+            return 0;
           }
-          return a * b
-
+          return a * b;
         } else if (sign === "÷") {
           if (a / b > Math.pow(2, 32)) {
-            alert("超過數字上限")
-            return 0
+            alert("超過數字上限");
+            return 0;
           }
-          return a / b
-
+          return a / b;
         }
-      }
+      };
 
-      localStorage.setItem("totalValue", getTotalValue())
-      localStorage.setItem("enteredValue", 0)
-      localStorage.setItem("sign", "")
       function getTotalValue() {
         if (count.enteredValue === "0" && count.sign === "÷") {
-          return "無法除以零"
+          alert("無法除以零");
+          return 0;
         } else {
-          return arithmetic(Number(count.totalValue), Number(count.enteredValue), count.sign)
+          return arithmetic(
+            Number(count.totalValue),
+            Number(count.enteredValue),
+            count.sign
+          );
         }
       }
+      const result = getTotalValue();
+
+      localStorage.setItem("totalValue", result);
+      localStorage.setItem("enteredValue", 0);
+      localStorage.setItem("sign", "");
+
       setCount({
         ...count,
-        totalValue: getTotalValue(),
+        totalValue: result,
         sign: "",
         enteredValue: 0,
-      })
+      });
     }
-  }
+  };
 
   const handleSignClick = (btnSymbol) => {
-
-
-
     function getTotalValue() {
       if (!count.totalValue && count.enteredValue) {
-        return count.enteredValue
-      } return count.totalValue
+        return count.enteredValue;
+      }
+      return count.totalValue;
     }
 
-    localStorage.setItem("totalValue", getTotalValue())
-    localStorage.setItem("enteredValue", 0)
-    localStorage.setItem("sign", btnSymbol)
+    localStorage.setItem("totalValue", getTotalValue());
+    localStorage.setItem("enteredValue", 0);
+    localStorage.setItem("sign", btnSymbol);
 
     setCount({
       ...count,
       sign: btnSymbol,
       enteredValue: 0,
-      totalValue: getTotalValue()
+      totalValue: getTotalValue(),
     });
-  }
+  };
 
   const handleClear = () => {
-
-    localStorage.setItem("totalValue", 0)
-    localStorage.setItem("enteredValue", 0)
-    localStorage.setItem("sign", "")
+    localStorage.setItem("totalValue", 0);
+    localStorage.setItem("enteredValue", 0);
+    localStorage.setItem("sign", "");
     setCount({
       ...count,
       enteredValue: 0,
       totalValue: 0,
       sign: "",
     });
-  }
+  };
 
   const handleReverse = () => {
-
     function getEnterValue() {
-      return count.enteredValue ? count.enteredValue * -1 : 0
+      return count.enteredValue ? count.enteredValue * -1 : 0;
     }
 
     function getTotalValue() {
-      return count.totalValue ? count.totalValue * -1 : 0
+      return count.totalValue ? count.totalValue * -1 : 0;
     }
-    localStorage.setItem("totalValue", getTotalValue())
-    localStorage.setItem("enteredValue", getEnterValue())
-    localStorage.setItem("sign", "")
+    localStorage.setItem("totalValue", getTotalValue());
+    localStorage.setItem("enteredValue", getEnterValue());
+    localStorage.setItem("sign", "");
     setCount({
       ...count,
       enteredValue: getEnterValue(),
       totalValue: getTotalValue(),
       sign: "",
     });
-  }
+  };
 
   const handlePercent = () => {
-
     function getEnteredValue() {
-      return (count.enteredValue ? count.enteredValue : 0) / 100
+      return (count.enteredValue ? count.enteredValue : 0) / 100;
     }
 
     function getTotalValue() {
-      return (count.totalValue ? count.totalValue : 0) / 100
+      return (count.totalValue ? count.totalValue : 0) / 100;
     }
-    localStorage.setItem("totalValue", getTotalValue())
-    localStorage.setItem("enteredValue", getEnteredValue())
-    localStorage.setItem("sign", "")
+    localStorage.setItem("totalValue", getTotalValue());
+    localStorage.setItem("enteredValue", getEnteredValue());
+    localStorage.setItem("sign", "");
     setCount({
       ...count,
       enteredValue: getEnteredValue(),
       totalValue: getTotalValue(),
       sign: "",
     });
-
-  }
+  };
 
   const handleSqrt = () => {
-
     function getEnteredValue() {
-      return Math.sqrt(count.enteredValue)
+      return Math.sqrt(count.enteredValue);
     }
 
     function getTotalValue() {
-      return Math.sqrt(count.totalValue)
+      return Math.sqrt(count.totalValue);
     }
 
-    localStorage.setItem("totalValue", getTotalValue())
-    localStorage.setItem("enteredValue", getEnteredValue())
-    localStorage.setItem("sign", "")
+    localStorage.setItem("totalValue", getTotalValue());
+    localStorage.setItem("enteredValue", getEnteredValue());
+    localStorage.setItem("sign", "");
     setCount({
       ...count,
       enteredValue: getEnteredValue(),
       totalValue: getTotalValue(),
       sign: "",
     });
-  }
+  };
 
   const handleDecimal = (btnSymbol) => {
-
     function enterDecimal() {
       if (count.enteredValue.toString().includes(".")) {
-        return count.enteredValue
+        return count.enteredValue;
       } else {
-        return count.enteredValue + btnSymbol
+        return count.enteredValue + btnSymbol;
       }
     }
 
-    localStorage.setItem("enteredValue", enterDecimal())
+    localStorage.setItem("enteredValue", enterDecimal());
     setCount({
       ...count,
-      enteredValue: enterDecimal()
+      enteredValue: enterDecimal(),
     });
-
-  }
+  };
 
   return (
     <div ref={dropRef}>
       <Wrapper>
-        <Screen count={count} value={count.enteredValue ? count.enteredValue : count.totalValue} />
+        <Screen
+          count={count}
+          value={count.enteredValue ? count.enteredValue : count.totalValue}
+        />
         <ButtonArea>
-          {
-            btnSymbols.map((btn, i) => {
-              return (
-                <Button
-                  key={i}
-                  value={btn} handleClick={handleClick}
-                />
-              );
-            })
-          }
+          {btnSymbols.map((btn, i) => {
+            return <Button key={i} value={btn} handleClick={handleClick} />;
+          })}
         </ButtonArea>
       </Wrapper>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +11,5 @@ ReactDOM.render(
       <App />
     </DndProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
